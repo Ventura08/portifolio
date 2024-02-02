bf2cca439691f8e98b757af548380eac382d1c69
Ventura08 <estevaotevu08@gmail.com>
2024-02-01 23:17:37 -0300

    instalacao css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={montserrat.className}>{children}</body>
     </html>
   );
