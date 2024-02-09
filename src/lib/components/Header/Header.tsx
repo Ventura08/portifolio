c7bda5e628c6e305a746e7526f55c33bb44f7d0d
Ventura08 <estevaotevu08@gmail.com>
2024-02-08 22:56:58 -0300

    .

--- src/lib/components/Header/Header.tsx
+++ src/lib/components/Header/Header.tsx
@@ -1,11 +1,37 @@
 import { tv } from 'tailwind-variants'
 
-const header = tv({
-    base: 'flex justify-center text-white text-lg xl:text-xl sm:text-sm px-6 py-4 animate-topToBottomFadeIn',
-})
-
+const header = tv(
+    {
+        base: 'transition-all flex justify-center text-white text-sm border-2 border-gray-50 mx-6 my-4 px-6 py-4 animate-topToBottomFadeIn',
+        variants: {
+            size: {
+                small: 'text-sm',
+                medium: 'text-base border-none',
+                large: 'text-lg border-none',
+                extraLarge: 'text-xl border-none',
+            },
+        },
+    },
+    {
+        responsiveVariants: true,
+    }
+)
 export const Header = () => {
     const text =
         '"Em um mundo de constantes atualizações, ser adaptável é o nosso maior poder. - Inspirado em Hunter x Hunter"'
-    return <header className={header()}>{text}</header>
+    return (
+        <header
+            className={header({
+                size: {
+                    sm: 'small',
+                    md: 'medium',
+                    lg: 'large',
+                    xl: 'extraLarge',
+                    '2xl': 'extraLarge',
+                },
+            })}
+        >
+            {text}
+        </header>
+    )
 }
